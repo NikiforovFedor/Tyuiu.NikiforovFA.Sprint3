fb53af301ce0cf9076e1f167b17eee6fdcbe3e26
Fedor Nikiforov <Tyuiu.NikiforovFA@outlook.com>
2024-10-16 16:18:07 +0500

    Task5

--- Tyuiu.NikiforovFA.Sprint3.Task5.V13.Test/DataServiceTest.cs
+++ Tyuiu.NikiforovFA.Sprint3.Task5.V13.Test/DataServiceTest.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.NikiforovFA.Sprint3.Task5.V13.Test
         public void GetSumSumSeriesValid()
         {
             DataService ds = new DataService();
-            Assert.AreEqual(ds.GetSumSumSeries(5, 1, 4, 1, 11), 144.487);
+            Assert.AreEqual(ds.GetSumSumSeries(5, 1, 4, 1, 11), 32.269);
         }
     }
 }
