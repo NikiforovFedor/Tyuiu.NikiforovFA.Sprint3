e66c6481edd0c3d8dee2a82aebe0cf2ccf4b8ae8
Fedor Nikiforov <Tyuiu.NikiforovFA@outlook.com>
2024-10-26 18:43:50 +0500

    Task6

--- Tyuiu.NikiforovFA.Sprint3.Task6.V24.Lib/DataService.cs
+++ Tyuiu.NikiforovFA.Sprint3.Task6.V24.Lib/DataService.cs
@@ -4,6 +4,15 @@ namespace Tyuiu.NikiforovFA.Sprint3.Task6.V24.Lib
 {
     public class DataService : ISprint3Task6V24
     {
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            int res = 0;
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                res += del(i);
+            }
+            return res;
+        }
         public static int del(int x)
         {
             int k = 0;
@@ -15,15 +24,6 @@ namespace Tyuiu.NikiforovFA.Sprint3.Task6.V24.Lib
                 }
             }
             return k;
-        }
-        public int GetSumTheDivisors(int startValue, int stopValue)
-        {
-            int res = 0;
-            for (int i = startValue; i <= stopValue; i++)
-            {
-                res += del(i);
-            }
-            return res;
         }
 
     }
